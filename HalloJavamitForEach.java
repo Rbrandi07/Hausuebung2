8cb7282cf1df3dae2ea7e38104521b927847daa0
Rbrandi07 <145335108+Rbrandi07@users.noreply.github.com>
2023-09-25 12:55:12 +0200

    Add files via upload

---
+++ HalloJavamitForEach.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavamitForEach {
+    public static void main(String[] args){
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Wort1");
+        stringList.add("Wort2");
+        stringList.add("Wort3");
+        stringList.add("Wort4");
+        stringList.add("MoneyBoy");
+    }
+}
